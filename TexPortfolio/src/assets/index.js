721c7697579a561e2e52321a256a7313dcd54834
Lenecplusultra <yytbrayan@gmail.com>
2025-10-05 20:35:11 -0400

    updating assets

--- TexPortfolio/src/assets/index.js
+++ TexPortfolio/src/assets/index.js
@@ -4,6 +4,7 @@ import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
 import projectFour from "./images/projects/projectFour.jpg";
+import projectFour from "./images/projects/projectFive.jpg";
 import contactImg from "./images/contact/contactImg.png";
 
 export {
